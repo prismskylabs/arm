94d0159bcc4320741f48e119d073ca591c1023e5
Huu Nguyen <whoshuu@gmail.com>
2016-12-01 15:20:03 -0800

    Update toolchain file

--- Toolchain.cmake
+++ Toolchain.cmake
@@ -1,13 +1,18 @@
 set(_target_name arm-linux-gnueabihf)
 set(_target_root ${CMAKE_CURRENT_LIST_DIR}/gcc-linaro-5.3.1-2016.05-x86_64_arm-linux-gnueabihf)
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfloat-abi=hard -mfpu=neon -mcpu=cortex-a9 -O2")
+set(CROSS_COMPILE_PREFIX ${_target_root}/bin/${_target_name}-)
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv7-a -mthumb-interwork -mfloat-abi=hard -mfpu=neon -mtune=cortex-a9 -mfloat-abi=hard -O2")
 set(CMAKE_CXX_FLAGS ${CMAKE_C_FLAGS})
 
-set(CMAKE_C_COMPILER ${_target_root}/bin/${_target_name}-gcc)
-set(CMAKE_CXX_COMPILER ${_target_root}/bin/${_target_name}-g++)
-set(CMAKE_INCLUDE_PATH ${_target_root}/${_target_name}/include)
-set(CMAKE_LIBRARY_PATH ${_target_root}/${_target_name}/lib)
+set(CMAKE_C_COMPILER ${CROSS_COMPILE_PREFIX}gcc)
+set(CMAKE_CXX_COMPILER ${CROSS_COMPILE_PREFIX}g++)
+
+set(CMAKE_FIND_ROOT_PATH ${_target_root}/${_target_name}/libc)
 
 if(CCACHE)
     set(CMAKE_C_COMPILER ${CCACHE} ${CMAKE_C_COMPILER})
